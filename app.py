fd488490eb2bd60fc8d2a665f9e1dd8ea56ddf41
Luis Mata <luismata5656@gmail.com>
2021-02-03 14:22:54 -0700

    Added basic movement

--- app.py
+++ app.py
@@ -1,42 +1,75 @@
 import pygame
 from pygame.locals import *
 
-class App:
+pygame.init()
 
+vec = pygame.math.Vector2
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -.12
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        self._running = True
-        self._display_surf = None
-        self.size = self.weight, self.height = 640, 400
+        super().__init__()
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128,255,40))
+        self.rect = self.surf.get_rect()
 
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._running = True
+        self.pos = vec((10, 385))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
 
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self._running = False
+    def move(self):
+        self.acc = vec(0,0)
 
-    def on_loop(self):
-        pass
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC
 
-    def on_render(self):
-        pass
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
 
-    def on_cleanup(self):
-        pygame.quit()
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
 
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
+        self.rect.midbottom = self.pos
 
-        while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
+class platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf.fill((255,0,0))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
 
-if __name__ == "__main__" :
-    theApp = App()
-    theApp.on_execute()
+PT1 = platform()
+P1 = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    displaysurface.fill((0,0,0))
+
+    P1.move()
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
