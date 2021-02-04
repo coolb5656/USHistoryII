e832007b6b5d8abc81554309bac0547410614a2f
LondonRal <landonau17@gmail.com>
2021-02-04 14:34:26 -0700

    added graphics

--- app.py
+++ app.py
@@ -2,30 +2,37 @@ import pygame
 from pygame.locals import *
 import sys
 import random
+import math
 
 pygame.init()
 vec = pygame.math.Vector2 #2 for two dimensional
 
-HEIGHT = 450
-WIDTH = 400
+HEIGHT = 1080
+WIDTH = 1920
 ACC = 0.5
-FRIC = -0.12
+FRIC = -0.02
 FPS = 60
 
+MAX_RATE = 10
+GROWTH_RATE = .02
+
 FramePerSec = pygame.time.Clock()
+
 
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game")
 
+bg = pygame.image.load("pics/bg.jpg").convert()
+bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        #self.image = pygame.image.load("character.png")
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((255,255,0))
+        self.surf = pygame.image.load("pics/US.png").convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (64, 64))
         self.rect = self.surf.get_rect()
 
-        self.pos = vec((10, 360))
+        self.pos = vec((250, HEIGHT - 250))
         self.vel = vec(0,0)
         self.acc = vec(0,0)
         self.jumping = False
@@ -60,12 +67,54 @@ class Player(pygame.sprite.Sprite):
                     self.vel.y = 0
                     self.jumping = False
 
+class Player2(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.image.load("pics/soviet.png").convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (64, 64))
+        self.rect = self.surf.get_rect()
+
+        self.pos = vec((WIDTH - 250, HEIGHT - 250))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.jumping = False
+
+    def move(self):
+        self.acc = vec(0,0)
+
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[K_a]:
+            self.acc.x = -ACC
+        if pressed_keys[K_d]:
+            self.acc.x = ACC
+
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
+
+    def update(self):
+        hits = pygame.sprite.spritecollide(self ,platforms, False)
+        if self.vel.y > 0:
+            if hits:
+                if self.pos.y < hits[0].rect.bottom:
+                    self.pos.y = hits[0].rect.top +1
+                    self.vel.y = 0
+                    self.jumping = False
+
 
 class platform(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.surf = pygame.Surface((random.randint(50,100), 12))
-        self.surf.fill((0,255,0))
+        self.surf = pygame.image.load("pics/rock.png").convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (64, 64))
         self.rect = self.surf.get_rect(center = (x, y))
 
     def move(self):
@@ -84,16 +133,19 @@ def check(platform, groupies):
         C = False
 
 P1 = Player()
+P2 = Player2()
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
+all_sprites.add(P2)
 
 platforms = pygame.sprite.Group()
 
-pl = platform(50, 50)
-platforms.add(pl)
-all_sprites.add(pl)
 
+
+rate = 1
+
+pygame.time.set_timer(100, 1000)
 
 while True:
     P1.update()
@@ -101,11 +153,18 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == 100:
+            pl = platform(random.randrange(0, WIDTH), 50)
+            platforms.add(pl)
+            all_sprites.add(pl)
+
+        if rate < MAX_RATE:
+            rate += rate * GROWTH_RATE
 
     for plat in platforms:
-        plat.rect.y += 1
+        plat.rect.y += rate
 
-    displaysurface.fill((0,0,0))
+    displaysurface.blit(bg, (0,0))
 
     for entity in all_sprites:
         displaysurface.blit(entity.surf, entity.rect)
@@ -113,3 +172,154 @@ while True:
 
     pygame.display.update()
     FramePerSec.tick(FPS)
+
+import pygame
+import time
+import random
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+block_color = (53,115,255)
+
+car_width = 73
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load('racecar.png')
+
+
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: "+str(count), True, black)
+    gameDisplay.blit(text,(0,0))
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+def car(x,y):
+    gameDisplay.blit(carImg,(x,y))
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+
+
+def crash():
+    message_display('You Crashed')
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
+        clock.tick(15)
+
+
+
+
+
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 4
+    thing_width = 100
+    thing_height = 100
+
+    thingCount = 1
+
+    dodged = 0
+
+    gameExit = False
+
+    while not gameExit:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+        gameDisplay.fill(white)
+
+        things(thing_startx, thing_starty, thing_width, thing_height, block_color)
+
+
+
+        thing_starty += thing_speed
+        car(x,y)
+        things_dodged(dodged)
+
+        if x > display_width - car_width or x < 0:
+            crash()
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
+
+        if y < thing_starty+thing_height:
+            print('y crossover')
+
+            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+                print('x crossover')
+                crash()
+
+        pygame.display.update()
+        clock.tick(60)
+
+game_intro()
+game_loop()
+pygame.quit()
+quit()
